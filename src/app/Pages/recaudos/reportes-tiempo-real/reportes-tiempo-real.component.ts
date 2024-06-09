16e90cade88fbf10e371536af6e7f2e69676cf19
mjvizuete2 <majovizuete24@gmail.com>
2024-06-08 22:17:47 -0500

    reportes tiempo real

--- src/app/Pages/recaudos/reportes-tiempo-real/reportes-tiempo-real.component.ts
+++ src/app/Pages/recaudos/reportes-tiempo-real/reportes-tiempo-real.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables  } from 'chart.js';
+import { RecaudosService } from 'src/app/Services/recaudos.service';
 
 @Component({
   selector: 'app-reportes-tiempo-real',
@@ -6,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reportes-tiempo-real.component.css']
 })
 export class ReportesTiempoRealComponent implements OnInit{
+  chart: any;
   usuario:any;
+  public recaudos: any[] = [];
 
-  constructor() { }
+
+  constructor(
+    private recaudosService: RecaudosService
+  ) { Chart.register(...registerables);}
 
 
 
@@ -19,6 +26,52 @@ export class ReportesTiempoRealComponent implements OnInit{
 
   ngOnInit(): void {
     this.usuario = this.getItem('usuario');
+    this.cargarReportesRecaudos();
+  }
+
+
+  cargarReportesRecaudos(): void {
+    this.recaudosService.reportesRecaudodsConsultar().subscribe(
+      (res) => {
+        if (res.status === 'success' && res.data && res.data.recaudos) {
+          this.recaudos = res.data.recaudos;
+          console.log('Respuesta reportes duarios:', this.recaudos);
+          this.createChart(this.recaudos);
+        } else {
+          console.error('Estructura de datos inesperada:', res);
+        }
+      },
+      (error) => {
+        console.error('Error al cargar cobros:', error);
+      }
+    );
+  }
+
+  createChart(recaudos: any[]) {
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    const montos = recaudos.map(recaudo => parseFloat(recaudo.monto));
+    const labels = recaudos.map(recaudo => recaudo.fechaInicio);
+
+    this.chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Montos',
+          data: montos,
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
   }
 
 }
