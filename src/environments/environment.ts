3bcded34f5350c5b4c43a27d2f9132b255982fc1
mjvizuete2 <majovizuete24@gmail.com>
2024-06-24 18:40:19 -0500

    correccion https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  CoreBanquito_account: 'http://corebanquito-account.us-east-1.elasticbeanstalk.com',
-  CoreBanquito_bank : 'http://corebanquito-bank.us-east-1.elasticbeanstalk.com',
-  CoreBanquito_client : 'http://corebanquito-client.us-east-1.elasticbeanstalk.com',
-  CoreBanquito_interest : 'http://corebanquito-interest.us-east-2.elasticbeanstalk.com',
-  CoreBanquito_product : 'http://corebanquito-product.us-east-2.elasticbeanstalk.com',
-  CoreCobros_commission : 'http://corecobros-commission.us-east-2.elasticbeanstalk.com',
-  CoreCobros_invoice : 'http://corecobros-invoice.us-east-1.elasticbeanstalk.com',
-  CoreCobros_receivables : 'http://corecobros-receivables.us-east-1.elasticbeanstalk.com',
+  CoreBanquito_account: 'https://corebanquito-account.us-east-1.elasticbeanstalk.com',
+  CoreBanquito_bank : 'https://corebanquito-bank.us-east-1.elasticbeanstalk.com',
+  CoreBanquito_client : 'https://corebanquito-client.us-east-1.elasticbeanstalk.com',
+  CoreBanquito_interest : 'https://corebanquito-interest.us-east-2.elasticbeanstalk.com',
+  CoreBanquito_product : 'https://corebanquito-product.us-east-2.elasticbeanstalk.com',
+  CoreCobros_commission : 'https://corecobros-commission.us-east-2.elasticbeanstalk.com',
+  CoreCobros_invoice : 'https://corecobros-invoice.us-east-1.elasticbeanstalk.com',
+  CoreCobros_receivables : 'https://corecobros-receivables.us-east-1.elasticbeanstalk.com',
 
   // CoreCobros_receivables: 'http://localhost:8080',
   // CoreBanquito_bank: 'http://localhost:8082',
